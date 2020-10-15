f341566b658f5c18ac988e8049123695676aa105
GeorgeChen <xiaomulv@hotmail.com>
2020-10-15 23:37:14 +0800

    init code redux

--- Client/src/index.jsx
+++ Client/src/index.jsx
@@ -10,20 +10,23 @@ import {
   Route,
   Redirect
 } from "react-router-dom";
+import { Provider } from 'react-redux'
 import Users from './pages/Users'
-
+//bac27dd40cb6f6de46292cc30a926d06f317c93d
 ReactDOM.render(
   <React.StrictMode>
-    <Router>
-      <BasicLayout {...defaultProps}>
-        <Switch>
-          <Route path="/users">
-            <Users />
-          </Route>
-          <Redirect exact from="/" to="/monitor" />
-        </Switch>
-      </BasicLayout>
-    </Router>
+    <Provider>
+      <Router>
+        <BasicLayout {...defaultProps}>
+          <Switch>
+            <Route path="/users">
+              <Users />
+            </Route>
+            <Redirect exact from="/" to="/monitor" />
+          </Switch>
+        </BasicLayout>
+      </Router>
+    </Provider>
   </React.StrictMode>,
   // eslint-disable-next-line no-undef
   document.getElementById('root'),
