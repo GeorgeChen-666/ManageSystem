9d8e186887e2874cb861a37dbe019adb550e3d1f
GeorgeChen-666 <xiaomulv@hotmail.com>
2020-11-02 18:27:47 +0800

    fix bug

--- Client/src/common/route.jsx
+++ Client/src/common/route.jsx
@@ -1,21 +1,28 @@
 import React from 'react';
 import Loadable from 'react-loadable';
 import _ from 'lodash';
-import { BrowserRouter, Redirect, Route, Switch } from 'react-router-dom';
+import {BrowserRouter, Redirect, Route, Switch} from 'react-router-dom';
 import DefaultLayout from '../components/Layout/DefaultLayout';
-import {routeMenus} from './menu';
+import {routeItems} from './menu';
+
 function getFlatMenuData(menus) {
   let keys = [];
   menus.forEach((item) => {
-    const newItem = { ...item };
+    const newItem = {...item};
     if (typeof newItem.component === 'object') {
       const componentPromise = newItem.component;
       newItem.component = Loadable({
         loader: () => componentPromise,
         loading: () => <div>loading...</div>,
       });
-    } else if (newItem.component) {
-      newItem.component = () => <div>{newItem.component}</div>;
+    } else if (typeof newItem.layout === 'object') {
+      const layoutPromise = newItem.layout;
+      newItem.layout = Loadable({
+        loader: () => layoutPromise,
+        loading: () => <div>loading...</div>,
+      });
+    } else {
+      return
     }
     keys.push(newItem);
     if (item.routes) {
@@ -24,8 +31,7 @@ function getFlatMenuData(menus) {
   });
   return keys;
 }
-
-const menuItemData = getFlatMenuData(_.get(routeMenus, ['route', 'routes'], []));
+const menuItemData = getFlatMenuData(_.get(routeItems, ['routes'], []));
 
 export const Router = (
   <BrowserRouter>
@@ -36,13 +42,13 @@ export const Router = (
           if (!!Component) {
             return (
               <Route key={item.path} path={item.path}>
-                <Component />
+                <Component/>
               </Route>
             );
           }
           return null;
         })}
-        <Redirect exact from="/" to="/manage/monitor" />
+        <Redirect exact from="/" to="/manage/monitor"/>
       </Switch>
     </DefaultLayout>
   </BrowserRouter>
