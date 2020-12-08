29c64067c75bef7a4d36941ddc51a0a86b37835d
GeorgeChen-666 <xiaomulv@hotmail.com>
2020-12-08 17:58:20 +0800

    change creator

--- Client/src/pages/Processes/ManageProcesses/ProcessCreator.jsx
+++ Client/src/pages/Processes/ManageProcesses/ProcessCreator.jsx
@@ -1,20 +1,22 @@
-import React, { useRef,useState } from 'react';
-import { Checkbox, Input, Modal, Dropdown, Button, Select } from 'antd';
-import { Link, matchPath, useRouteMatch, useHistory } from 'react-router-dom';
+import React, {useRef, useState} from 'react';
+import {Checkbox, Input, Modal, Dropdown, Button, Select} from 'antd';
+import {Link, matchPath, useRouteMatch, useHistory} from 'react-router-dom';
 import ValidationForm from '../../../components/Form/ValidationForm';
 import FormField from '../../../components/Form/FormField';
-import { DownOutlined } from '@ant-design/icons';
+import {DownOutlined} from '@ant-design/icons';
 import styles from '../Style.module.less';
 import mime from 'mime';
-import { FilePond, registerPlugin } from 'react-filepond';
+import {FilePond, registerPlugin} from 'react-filepond';
 import fpfvs from 'filepond-plugin-file-validate-size';
+
 registerPlugin(fpfvs);
-const { Option } = Select;
+const allowedExtensions = [ 'js', 'exe', 'bat', 'cmd', 'sh', 'jar', 'class', 'zip'];
+const {Option} = Select;
 //fileRef.current.getFile().file
 export default (props) => {
   const history = useHistory();
   const formRef = useRef({});
-  const [fileObject,setFileObject] = useState();
+  const [fileObject, setFileObject] = useState();
   const fileRef = useRef({});
   console.log(mime.getType('jar'))
   const data = {};
@@ -28,58 +30,78 @@ export default (props) => {
       destroyOnClose={true}
       className={styles.terminalModal}
     >
-      <ValidationForm ref={formRef} name={props.name} onSubmit={() => {}}>
+      <ValidationForm ref={formRef} name={props.name} onSubmit={() => {
+      }}>
         <FormField name="id" hidden={true} initialValue={data.id}>
-          <Input />
+          <Input/>
         </FormField>
         <FormField
           name="name"
           required={true}
           label={'服务名称'}
-          labelCol={{ span: 5 }}
+          labelCol={{span: 5}}
           initialValue={data.nickname}
         >
-          <Input />
+          <Input/>
         </FormField>
         <FormField
           name="file"
-          required={true}
-          labelCol={{ span: 5 }}
+          labelCol={{span: 5}}
           label={'服务文件'}
-          style={{ textAlign: 'right' }}
+          style={{textAlign: 'right'}}
           trigger={'onupdatefiles'}
+          rules={[
+            {
+              validator: async (rule, value) => {
+                const [file = {}] = value;
+                const {fileExtension} = file;
+                if (![undefined,...allowedExtensions].includes(fileExtension)) {
+                  return Promise.reject(new Error('文件类型不支持！'));
+                }
+              }
+            }
+          ]}
         >
           <FilePond
-            // onupdatefiles={(fileItems)=>{
-            //   setFileObject(fileItems[0])
-            // }}
-            // onchage={()=>{
-            //   console.log('=')
-            // }}
+            labelIdle={`拖拽文件至此或 <span class="filepond--label-action">浏览</span><br />支持格式：${allowedExtensions.join(',')}`}
             allowMultiple={false}
             maxFiles={1}
           />
         </FormField>
         <FormField
+          name="exe"
+          label={'应用程序'}
+          labelCol={{span: 5}}
+          initialValue={data.cmd}
+        >
+          <Select style={{ width: 240 }}>
+            <Option value="jack">Jack</Option>
+            <Option value="lucy">Lucy</Option>
+            <Option value="Yiminghe">yiminghe</Option>
+          </Select>
+        </FormField>
+        <FormField
           name="param"
-          required={true}
           label={'附加参数'}
-          labelCol={{ span: 5 }}
+          labelCol={{span: 5}}
           initialValue={data.username}
         >
-          <Input />
+          <Input/>
         </FormField>
         <FormField
           name="isAutoStart"
           valuePropName="checked"
           label={'自动启动'}
-          labelCol={{ span: 5 }}
+          labelCol={{span: 5}}
           initialValue={data.permissionType}
         >
-          <Checkbox />
+          <Checkbox/>
         </FormField>
       </ValidationForm>
-      <Button onClick={()=>{console.log(fileRef);debugger;}}>????</Button>
+      <Button onClick={() => {
+        console.log(fileRef);
+        debugger;
+      }}>????</Button>
     </Modal>
   );
 };
